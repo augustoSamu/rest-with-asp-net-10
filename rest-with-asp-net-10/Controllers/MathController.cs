fdecc9a2802c3c5d7f9538d0cfe7860ca6501e51
augustoSamu <samuca.augusto05@gmail.com>
2026-04-12 21:52:21 -0300

    feat: add math operations

--- rest-with-asp-net-10/Controllers/MathController.cs
+++ rest-with-asp-net-10/Controllers/MathController.cs
@@ -7,15 +7,85 @@ namespace rest_with_asp_net_10.Controllers
     public class MathController : ControllerBase
     {
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string firstNumber, string secondNumber)
+        public IActionResult Sum(string firstNumber, string secondNumber)
         {
             if (!IsNumeric(firstNumber) || !IsNumeric(secondNumber))
             {
                 return BadRequest("Invalid Input!");
             }
 
-            var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
-            return Ok(sum);
+            var result = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+            return Ok(result);
+        }
+
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtraction(string firstNumber, string secondNumber)
+        {
+            if (!IsNumeric(firstNumber) || !IsNumeric(secondNumber))
+            {
+                return BadRequest("Invalid Input!");
+            }
+
+            var result = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+            return Ok(result);
+        }
+
+        [HttpGet("mult/{firstNumber}/{secondNumber}")]
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if (!IsNumeric(firstNumber) || !IsNumeric(secondNumber))
+            {
+                return BadRequest("Invalid Input!");
+            }
+
+            var result = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+            return Ok(result);
+        }
+
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public IActionResult Division(string firstNumber, string secondNumber)
+        {
+            if (!IsNumeric(firstNumber) || !IsNumeric(secondNumber))
+            {
+                return BadRequest("Invalid Input!");
+            }
+
+            if (firstNumber == "0" || firstNumber == "0")
+            {
+                return BadRequest("Cannot division by zero!");
+            }
+
+            var result = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+            return Ok(result);
+        }
+
+        [HttpGet("avg/{firstNumber}/{secondNumber}")]
+        [HttpGet("average/{firstNumber}/{secondNumber}")]
+        public IActionResult Average(string firstNumber, string secondNumber)
+        {
+            if (!IsNumeric(firstNumber) || !IsNumeric(secondNumber))
+            {
+                return BadRequest("Invalid Input!");
+            }
+
+            var result = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+            return Ok(result);
+        }
+
+        [HttpGet("sqrt/{number}")]
+        [HttpGet("square-root/{number}")]
+        public IActionResult SquareRoot(string number)
+        {
+            if (!IsNumeric(number))
+            {
+                return BadRequest("Invalid Input!");
+            }
+
+            var result = Math.Sqrt((double)ConvertToDecimal(number));
+            return Ok(result);
         }
 
         private decimal ConvertToDecimal(string strNumber)
@@ -23,7 +93,7 @@ namespace rest_with_asp_net_10.Controllers
             if (decimal.TryParse(
                 strNumber,
                 System.Globalization.NumberStyles.Any,
-                System.Globalization.NumberFormatInfo.InvariantInfo, 
+                System.Globalization.NumberFormatInfo.InvariantInfo,
                 out decimal value))
             {
                 return value;
@@ -35,9 +105,9 @@ namespace rest_with_asp_net_10.Controllers
         private bool IsNumeric(string strNumber)
         {
             return decimal.TryParse(
-                strNumber, 
-                System.Globalization.NumberStyles.Any, 
-                System.Globalization.NumberFormatInfo.InvariantInfo, 
+                strNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
                 out decimal value);
         }
     }
