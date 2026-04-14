f53a6d7ebd3f8881e68a052b0e139b5f25cc2015
augustoSamu <samuca.augusto05@gmail.com>
2026-04-13 23:52:13 -0300

    feat: add person model and servise

---
+++ rest-with-asp-net-10/Services/IPersonService.cs
@@ -0,0 +1,17 @@
+﻿using rest_with_asp_net_10.Model;
+
+namespace rest_with_asp_net_10.Services
+{
+    public interface IPersonService
+    {
+        Person FindById(int id);
+
+        List<Person> FindAll();
+
+        Person Create(Person person);
+
+        Person Update(Person person);
+
+        void Delete(int id);
+    }
+}
---
+++ rest-with-asp-net-10/Services/PersonService.cs
@@ -0,0 +1,56 @@
+﻿using rest_with_asp_net_10.Model;
+
+namespace rest_with_asp_net_10.Services
+{
+    public class PersonService : IPersonService
+    {
+        public Person FindById(int id)
+        {
+            return MockPerson(id);
+        }
+        
+        public List<Person> FindAll()
+        {
+            return new List<Person>
+            {
+                MockPerson(new Random().Next(1, 1000)),
+                MockPerson(new Random().Next(1, 1000)),
+                MockPerson(new Random().Next(1, 1000)),
+                MockPerson(new Random().Next(1, 1000)),
+                MockPerson(new Random().Next(1, 1000)),
+                MockPerson(new Random().Next(1, 1000)),
+                MockPerson(new Random().Next(1, 1000)),
+                MockPerson(new Random().Next(1, 1000))
+            };
+        }
+
+        public Person Create(Person person)
+        {
+            person.Id = new Random().Next(1, 1000);
+            return person;
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(int id)
+        {
+
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = new Random().Next(1, 1000),
+                FirstName = $"John{i}",
+                LastName = $"Doe{i}",
+                Address = $"123 Main{i}",
+                Gender = i % 2 == 0 ? "Male" : "Female"
+            };
+        }
+
+    }
+}
