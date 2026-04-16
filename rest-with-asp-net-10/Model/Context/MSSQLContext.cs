e3dd0a3268eaaf22c519f2a0fcf9f30ea9b2935e
augustoSamu <samuca.augusto05@gmail.com>
2026-04-15 23:31:12 -0300

    feat: remove old examples and start connection in mssql

---
+++ rest-with-asp-net-10/Model/Context/MSSQLContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace rest_with_asp_net_10.Model.Context
+{
+    public class MSSQLContext : DbContext
+    {
+        public MSSQLContext(DbContextOptions<MSSQLContext> options) 
+            : base(options) { }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
--- rest-with-asp-net-10/Model/Gretting.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace rest_with_asp_net_10.Model
-{
-    public record Gretting(long id, string content);
-}
