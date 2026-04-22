1334fdee9414cd0458b199405f801bc637a43f8b
augustoSamu <samuca.augusto05@gmail.com>
2026-04-21 22:01:05 -0300

    feat: create script to migration and seeds

---
+++ rest-with-asp-net-10/db/seeds/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT [dbo].[person] ([first_name], [last_name], [address], [gender]) 
+VALUES (N'Samuca', N'Silva', N'sao paulo - sp', N'macho');
+
+INSERT [dbo].[person] ([first_name], [last_name], [address], [gender]) 
+VALUES (N'Kratos', N'deus da guera', N'atenaa', N'conan');
+
+INSERT [dbo].[person] ([first_name], [last_name], [address], [gender]) 
+VALUES (N'vo max', N'veio irado', N'omni', N'veio');
