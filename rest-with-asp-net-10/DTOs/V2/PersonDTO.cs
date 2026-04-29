2c8d47aa75074b4a9cf7f2998f7ef5df9f2c8457
augustoSamu <samuca.augusto05@gmail.com>
2026-04-28 23:38:11 -0300

    feat: customise date

--- rest-with-asp-net-10/DTOs/V2/PersonDTO.cs
+++ rest-with-asp-net-10/DTOs/V2/PersonDTO.cs
@@ -1,27 +1,29 @@
-﻿using System.Text.Json.Serialization;
+﻿using rest_with_asp_net_10.JsonSerialization;
+using System.Text.Json.Serialization;
 
 namespace rest_with_asp_net_10.DTOs.V2
 {
     public class PersonDTO
     {
-        [JsonPropertyOrder(3)]
-        [JsonPropertyName("code")]
+        //[JsonPropertyOrder(3)]
+        //[JsonPropertyName("code")]
         public int Id { get; set; }
 
-        [JsonPropertyOrder(4)]
-        [JsonPropertyName("first_name")]
+        //[JsonPropertyOrder(4)]
+        //[JsonPropertyName("first_name")]
         public string FirstName { get; set; }
 
-        [JsonPropertyOrder(5)]
-        [JsonPropertyName("last_name")]
+        //[JsonPropertyOrder(5)]
+        //[JsonPropertyName("last_name")]
         public string LastName { get; set; }
 
-        [JsonPropertyOrder(1)]
+        //[JsonPropertyOrder(1)]
         public string Address { get; set; }
 
         public string Gender { get; set; }
 
-        [JsonPropertyOrder(2)]
+        //[JsonPropertyOrder(2)]
+        [JsonConverter(typeof(DateSerializer))]
         public DateTime? BirthDay { get; set; }
     }
 }
---
+++ rest-with-asp-net-10/JsonSerialization/DateSerializer.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace rest_with_asp_net_10.JsonSerialization
+{
+    public class DateSerializer : JsonConverter<DateTime?>
+    {
+        private readonly string _format = "dd/MM/yyyy";
+
+        public override DateTime? Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options)
+        {
+            if (DateTime.TryParseExact(reader.GetString(), _format, null, DateTimeStyles.None, out DateTime date))
+            {
+                return date;
+            }
+            return null;
+        }
+
+        public override void Write(
+            Utf8JsonWriter writer,
+            DateTime? value,
+            JsonSerializerOptions options)
+        {
+            if (value.HasValue)
+            {
+                writer.WriteStringValue(value.Value.ToString(_format));
+            }
+            else
+            {
+                writer.WriteNullValue();
+            }
+        }
+    }
+}
