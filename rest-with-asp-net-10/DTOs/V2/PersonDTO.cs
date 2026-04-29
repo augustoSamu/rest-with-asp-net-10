30cc6ee8ba09e92bebfb5da44ad63c577907abff
augustoSamu <samuca.augusto05@gmail.com>
2026-04-28 23:30:30 -0300

    feat: introduce custom json serialization

--- rest-with-asp-net-10/DTOs/V2/PersonDTO.cs
+++ rest-with-asp-net-10/DTOs/V2/PersonDTO.cs
@@ -1,12 +1,27 @@
-﻿namespace rest_with_asp_net_10.DTOs.V2
+﻿using System.Text.Json.Serialization;
+
+namespace rest_with_asp_net_10.DTOs.V2
 {
     public class PersonDTO
     {
+        [JsonPropertyOrder(3)]
+        [JsonPropertyName("code")]
         public int Id { get; set; }
+
+        [JsonPropertyOrder(4)]
+        [JsonPropertyName("first_name")]
         public string FirstName { get; set; }
+
+        [JsonPropertyOrder(5)]
+        [JsonPropertyName("last_name")]
         public string LastName { get; set; }
+
+        [JsonPropertyOrder(1)]
         public string Address { get; set; }
+
         public string Gender { get; set; }
+
+        [JsonPropertyOrder(2)]
         public DateTime? BirthDay { get; set; }
     }
 }
